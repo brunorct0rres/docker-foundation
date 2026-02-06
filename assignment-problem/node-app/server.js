a216f6023c417622fcce64eb3016425aba6be11e
Bruno Calixto <brunorctorres@gmail.com>
2026-02-06 08:57:14 -0400

    Trabalhando com diferentes volumes

--- assignment-problem/node-app/server.js
+++ assignment-problem/node-app/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.get('/data/:filename', (req, res) => {
+  const filePath = path.join(__dirname, 'data', req.params.filename);
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Arquivo não encontrado');
+    }
+  });
+});
+
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from inside the very basic Node app!</h1>
+    <h1>Hello from inside the very basic Node app...</h1>
   `);
 })
 
