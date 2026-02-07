ce103de7665d48e7346cf62356cdd7131d468576
Bruno Calixto <brunorctorres@gmail.com>
2026-02-07 11:17:08 -0400

    Adding env to Dockerfile to use in app

--- assignment-problem/node-app/server.js
+++ assignment-problem/node-app/server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/data/:filename', (req, res) => {
-  const filePath = path.join(__dirname, 'data', req.params.filename);
+app.get("/data/:filename", (req, res) => {
+  const filePath = path.join(__dirname, "data", req.params.filename);
   res.sendFile(filePath, (err) => {
     if (err) {
-      res.status(404).send('Arquivo não encontrado');
+      res.status(404).send("Arquivo não encontrado");
     }
   });
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
     <h1>Hello from inside the very basic Node app...</h1>
   `);
-})
+});
 
-app.listen(3000);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+});
